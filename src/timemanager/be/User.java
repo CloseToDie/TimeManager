e079fca0965f9e75178faff73752b0dfa8263f71
Andreas <55873124+CloseToDie@users.noreply.github.com>
2020-04-28 12:38:07 +0200

    Added javadoc and password field

--- src/timemanager/be/User.java
+++ src/timemanager/be/User.java
@@ -7,53 +7,90 @@ package timemanager.be;
 
 /**
  *
- * @author kaspe
+ * @author kaspe & andreasvillumsen
  */
-public class User
-{
+public class User {
+
     private int id;
     private String name;
     private String email;
+    private String password;
 
-    public User(int id, String name, String email)
-    {
+    /**
+     * User Constructor
+     * @param id
+     * @param name
+     * @param email 
+     */
+    public User(int id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
-                
+
     }
 
-    public void setId(int id)
-    {
-        this.id = id;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public void setEmail(String email)
-    {
-        this.email = email;
-    }
-
-    public int getId()
-    {
+    /**
+     * Get the user's id
+     * @return id
+     */
+    public int getId() {
         return id;
     }
 
-    public String getName()
-    {
+    /**
+     * Set the user's id
+     * @param id 
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Get the user's name
+     * @return name
+     */
+    public String getName() {
         return name;
     }
 
-    public String getEmail()
-    {
+    /**
+     * Set the user's name
+     * @param name 
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get the user's email
+     * @return email
+     */
+    public String getEmail() {
         return email;
     }
-    
-    
-    
-   
+
+    /**
+     * Set the user's email
+     * @param email 
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Get the user's password
+     * @return password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Set the user's password
+     * @param password 
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
