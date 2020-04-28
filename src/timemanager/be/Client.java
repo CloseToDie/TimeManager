f9a51f08db55e3b878c331857368572413fcc555
Andreas <55873124+CloseToDie@users.noreply.github.com>
2020-04-28 12:44:57 +0200

    Create Client BLL class

---
+++ src/timemanager/be/Client.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timemanager.be;
+
+/**
+ *
+ * @author andreasvillumsen
+ */
+public class Client {
+    private int id;
+    private String name;
+
+    /**
+     * Client Constructor
+     * @param id
+     * @param name 
+     */
+    public Client(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /**
+     * Get the client id
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Set the client id
+     * @param id 
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Get the client name
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the client name
+     * @param name 
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
