4156ca74c0a5b975fd17f4004a5be886546be76f
Andreas <55873124+CloseToDie@users.noreply.github.com>
2020-04-29 09:29:56 +0200

    AuthManager login

--- src/timemanager/dal/database/AuthManagerDBDAO.java
+++ src/timemanager/dal/database/AuthManagerDBDAO.java
@@ -1,23 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package timemanager.dal.database;
+
+import java.io.IOException;
+import timemanager.be.User;
+import timemanager.dal.UserManagerFacade;
+import timemanager.utils.auth.Password;
 
 /**
  *
  * @author andreasvillumsen
  */
 public class AuthManagerDBDAO {
+    UserManagerFacade um;
+
+    public AuthManagerDBDAO() throws IOException {
+        um = new UserManagerDBDAO();
+    }
     
-    //TODO
-    public boolean login(String user, String pass) {
+    public User login(String username, String pass) {
         // Find user from database.
-        
-        
+        User user = um.getUserByName(username);
         // Verify password
-        
-        return true;
+        if(passMatches(user, pass)) {
+            return user;
+        } else {
+            return null;
+        }
+    }
+    
+    public boolean passMatches(User user, String pass) {
+        return Password.verifyPassword(pass, user.getPassword(), 
+                user.getSalt(), "SHA-512");
     }
 }
