7011ea39456dbea5c207b3e2c1bc4489465a5bfa
Christian Hussmann <chri.hussmann@protonmail.com>
2020-05-12 19:04:58 +0200

    Client og user create

--- src/timemanager/gui/model/ClientModel.java
+++ src/timemanager/gui/model/ClientModel.java
@@ -1,9 +1,25 @@
 package timemanager.gui.model;
 
+import java.io.IOException;
+import timemanager.be.Client;
+import timemanager.bll.ClientManager;
+
+
+
 /**
  *
  * @author andreasvillumsen
  */
 public class ClientModel {
+    private ClientManager cm;
+    public ClientModel() throws IOException {
+        cm = new ClientManager();
+    }
+    public void storeClient(Client client){
+        cm.storeClient(client);
+        
+    }
     
+    
+
 }
